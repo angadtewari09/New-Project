41e907ee168a7c2d6d88ec4e9c4a597739ed651d
angadtewari09 <despacito0099@gmail.com>
2020-09-25 18:29:35 +0530

    working

--- script.js
+++ script.js
@@ -9,12 +9,21 @@ $(document).ready(function(){
         $(".container").fadeIn(1500);
     });
 });
+
 function blackjack() {
     let player = 0;
     let dealer = 0;
-    const hit_btn = document.getElementById("deal-1");
     const stand_btn = document.getElementById("deal-2");
     const deal_btn = document.getElementById("deal-3");
-
-    
+    document.getElementById("deal-1").addEventListener('click' , function(){
+        var deck = [ 2,3,4,5,6,7,8,9,10,'K','Q','J','A'];
+        var single = deck[Math.floor(Math.random()*deck.length)];
+        console.log(single);
+        let card = document.createElement('img');
+        card.classList.add("size");
+        card.src = `${single}.png`;
+        document.getElementById("your-area").appendChild(card);
+       
+    });
 }
+blackjack();
