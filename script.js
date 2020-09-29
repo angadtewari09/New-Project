859c467f9a693fb8bb2d4efc5644b62df13e7bc4
angadtewari09 <despacito0099@gmail.com>
2020-09-29 12:58:01 +0530

    more code added

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function blackjack() {
     const stand_btn = document.getElementById("deal-2");
     const deal_btn = document.getElementById("deal-3");
     document.getElementById("deal-1").addEventListener('click' , function(){
-        var deck = [ 2,3,4,5,6,7,8,9,10,'K','Q','J','A'];
+        var deck = [ 2,3,4,5,6,7,8,9,10,'J','Q','K','A'];
         var single = deck[Math.floor(Math.random()*deck.length)];
         console.log(single);
         let card = document.createElement('img');
@@ -37,7 +37,19 @@ function blackjack() {
             scrollTop: $('#your-area').get(0).scrollHeight}, 200); 
        
          /*matching the player-score*/
-         let playe_score = document.   
+         var player_score = 0;
+         if( (single === 'Q') || (single === 'J')  || (single === 'K')  )
+         {
+            player_score = player_score +  10 ;
+         }   
+         else if ( single === 'A' )
+        {
+            player_score = player_score + 11;
+        }  
+        else {
+            player_score = player_score + single;
+        }
+        console.log(player_score);
     });
     /*document.getElementById("deal-3").addEventListener('click' , function(){
         
